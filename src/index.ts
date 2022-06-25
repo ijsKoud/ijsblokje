83817e9de85cf78fe455732e72d41a8bfe7d4da0
ijsKoud <github@ijskoud.dev>
2022-06-25 21:19:26 +0200

    fix: eslint issues

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import type { Probot } from "probot";
 
 const run = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+	app.on("issues.opened", async (context) => {
+		const issueComment = context.issue({
+			body: "Thanks for opening this issue!"
+		});
+		await context.octokit.issues.createComment(issueComment);
+	});
+	// For more information on building apps:
+	// https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+	// To get your app running against GitHub, see:
+	// https://probot.github.io/docs/development/
 };
 
 export default run;
--- test/index.test.ts
+++ test/index.test.ts
@@ -8,60 +8,57 @@ import { Probot, ProbotOctokit } from "probot";
 // Requiring our fixtures
 import payload from "./fixtures/issues.opened.json";
 const issueCreatedBody = { body: "Thanks for opening this issue!" };
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
 
-const privateKey = fs.readFileSync(
-  path.join(__dirname, "fixtures/mock-cert.pem"),
-  "utf-8"
-);
+const privateKey = fs.readFileSync(path.join(__dirname, "fixtures/mock-cert.pem"), "utf-8");
 
 describe("My Probot app", () => {
-  let probot: any;
+	let probot: any;
 
-  beforeEach(() => {
-    nock.disableNetConnect();
-    probot = new Probot({
-      appId: 123,
-      privateKey,
-      // disable request throttling and retries for testing
-      Octokit: ProbotOctokit.defaults({
-        retry: { enabled: false },
-        throttle: { enabled: false },
-      }),
-    });
-    // Load our app into probot
-    probot.load(myProbotApp);
-  });
+	beforeEach(() => {
+		nock.disableNetConnect();
+		probot = new Probot({
+			appId: 123,
+			privateKey,
+			// disable request throttling and retries for testing
+			Octokit: ProbotOctokit.defaults({
+				retry: { enabled: false },
+				throttle: { enabled: false }
+			})
+		});
+		// Load our app into probot
+		probot.load(myProbotApp);
+	});
 
-  test("creates a comment when an issue is opened", async (done) => {
-    const mock = nock("https://api.github.com")
-      // Test that we correctly return a test token
-      .post("/app/installations/2/access_tokens")
-      .reply(200, {
-        token: "test",
-        permissions: {
-          issues: "write",
-        },
-      })
+	test("creates a comment when an issue is opened", async (done) => {
+		const mock = nock("https://api.github.com")
+			// Test that we correctly return a test token
+			.post("/app/installations/2/access_tokens")
+			.reply(200, {
+				token: "test",
+				permissions: {
+					issues: "write"
+				}
+			})
 
-      // Test that a comment is posted
-      .post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
-        done(expect(body).toMatchObject(issueCreatedBody));
-        return true;
-      })
-      .reply(200);
+			// Test that a comment is posted
+			.post("/repos/hiimbex/testing-things/issues/1/comments", (body: any) => {
+				done(expect(body).toMatchObject(issueCreatedBody));
+				return true;
+			})
+			.reply(200);
 
-    // Receive a webhook event
-    await probot.receive({ name: "issues", payload });
+		// Receive a webhook event
+		await probot.receive({ name: "issues", payload });
 
-    expect(mock.pendingMocks()).toStrictEqual([]);
-  });
+		expect(mock.pendingMocks()).toStrictEqual([]);
+	});
 
-  afterEach(() => {
-    nock.cleanAll();
-    nock.enableNetConnect();
-  });
+	afterEach(() => {
+		nock.cleanAll();
+		nock.enableNetConnect();
+	});
 });
 
 // For more information about testing with Jest see:
