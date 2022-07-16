4c6611b2faea25f4f92aa635d2da8eb5088be18c
ijsKoud <github@ijskoud.dev>
2022-07-16 11:53:16 +0200

    feat: add auto releaser on commit

---
+++ src/bot/github/Release.ts
@@ -0,0 +1,23 @@
+import { Changelog } from "../../lib/Release/Changelog";
+import type { Ijsblokje } from "../ijsblokje";
+
+export const Release = (bot: Ijsblokje) => {
+	const app = bot.probot.probotApp;
+	const changelogGenerator = new Changelog(bot);
+
+	app.on("push", async (ctx) => {
+		if (ctx.payload.repository.private) return;
+
+		const commit = ctx.payload.commits.find((commit) => commit.message.includes("chore(Release): v"));
+		if (!commit) return;
+
+		const version = changelogGenerator.getVersion(commit.message);
+		if (!version) return;
+
+		const repo = ctx.repo();
+		const latestCommit = await ctx.octokit.repos.getCommit({ ...repo, ref: "main" });
+		const changelog = await changelogGenerator.run(repo.owner, repo.repo, latestCommit.data.sha);
+
+		await ctx.octokit.repos.createRelease({ ...repo, tag_name: `v${version}`, name: version, body: changelog });
+	});
+};
--- src/bot/github/index.ts
+++ src/bot/github/index.ts
@@ -1,8 +1,10 @@
 import type { Ijsblokje } from "../ijsblokje";
+import { Release } from "./Release";
 import { Webhook } from "./Webhook";
 
 const LoadAll = (bot: Ijsblokje) => {
 	Webhook(bot);
+	Release(bot);
 };
 
 export default LoadAll;
