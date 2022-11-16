c5f9e592bb195861a8b01060c1fdff75ed77ea05
ijsKoud <github@ijskoud.dev>
2022-11-16 22:33:05 +0100

    feat: remove template Action

--- src/actions/*/*/GlobalAction.ts
+++
@@ -1,9 +0,0 @@
-import { ApplyActionOptions } from "../../../lib/Decorators/ActionDecorators.js";
-import { Action } from "../../../lib/Structures/Action.js";
-
-@ApplyActionOptions({ event: "push" })
-export default class GlobalAction extends Action {
-	public run(ctx: Action.Context<"push">) {
-		console.log(ctx.name, ctx.payload);
-	}
-}
