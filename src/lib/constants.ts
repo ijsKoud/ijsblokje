3a7fdd4b1cecc629779cd73b5ffe9f926468f82d
ijsKoud <github@ijskoud.dev>
2022-11-22 21:24:55 +0100

    feat: add Webhook syncing

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -25,3 +25,34 @@ export const COMMIT_TYPES = [
 ];
 export const COMMIT_REGEX =
 	/^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|¯\\_\(ツ\)_\/¯)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)/gm;
+
+export const WEBHOOK_EVENTS = [
+	"branch_protection_rule",
+	"code_scanning_alert",
+	"commit_comment",
+	"create",
+	"delete",
+	"discussion",
+	"discussion_comment",
+	"fork",
+	"gollum",
+	"issues",
+	"issue_comment",
+	"member",
+	"milestone",
+	"project",
+	"project_card",
+	"public",
+	"pull_request",
+	"pull_request_review",
+	"pull_request_review_comment",
+	"pull_request_review_thread",
+	"push",
+	"release",
+	"repository",
+	"repository_import",
+	"secret_scanning_alert",
+	"star",
+	"team_add",
+	"watch"
+];
