c49f94489c236772ccedc2cfd9564567dcf3074f
ijsKoud <github@ijskoud.dev>
2022-11-22 20:13:28 +0100

    feat: update readme when repo details are changed

--- src/actions/ijsKoud/*/ReadmeSync.ts
+++ src/actions/ijsKoud/*/ReadmeSync.ts
@@ -3,18 +3,63 @@ import { ApplyActionOptions } from "../../../lib/Decorators/ActionDecorators.js"
 import { Action } from "../../../lib/Structures/Action.js";
 
 @ApplyActionOptions({
-	events: ["push"]
+	events: ["push", "repository"]
 })
 export default class ReadmeSync extends Action {
-	public async run(ctx: Action.Context<"push">) {
-		const repo = ctx.repo();
+	public async run(_ctx: Action.Context<"push" | "repository.edited" | "repository.renamed">) {
+		const repo = _ctx.repo();
+		if (_ctx.name === "repository") {
+			const ctx = _ctx as any as Action.Context<"repository.edited" | "repository.renamed">;
+			await this.repoRun(ctx);
+			return;
+		}
+
+		const ctx = _ctx as any as Action.Context<"push">;
+		const isLicenseUpdate = ctx.payload.commits.some((c) => [...c.added, ...c.modified, ...c.removed].includes("LICENSE"));
 		if (repo.repo === repo.owner && ctx.payload.commits.some((cm) => cm.modified.includes(BASE_README))) {
 			await this.templateUpdate(ctx);
 			return;
 		}
 
-		if (!ctx.payload.commits.some((cm) => cm.modified.includes(README_CONFIG_LOCATION) || cm.added.includes(README_CONFIG_LOCATION))) return;
+		if (
+			!ctx.payload.commits.some((cm) => cm.modified.includes(README_CONFIG_LOCATION) || cm.added.includes(README_CONFIG_LOCATION)) &&
+			!isLicenseUpdate
+		)
+			return;
 		await this.configUpdate(ctx);
+	}
+
+	private async repoRun(ctx: Action.Context<"repository.edited" | "repository.renamed">) {
+		if (!["renamed", "edited"].includes(ctx.payload.action)) return;
+		const repo = ctx.repo();
+
+		try {
+			const readmeConfig = await ctx.octokit.repos
+				.getContent({
+					...repo,
+					path: README_CONFIG_LOCATION
+				})
+				.catch(() => null);
+			if (!readmeConfig || !("content" in readmeConfig.data)) return;
+
+			const readmeData = await ctx.octokit.repos.getContent({ owner: repo.owner, repo: repo.owner, path: BASE_README });
+			if (!("content" in readmeData.data)) return;
+
+			let readme = Buffer.from(readmeData.data.content, "base64").toString();
+			const content = Buffer.from(readmeConfig.data.content, "base64").toString();
+			const jsonContent = JSON.parse(content);
+			const keys = Object.keys(jsonContent);
+
+			keys.forEach((key) => (readme = readme.replaceAll(`{${key}}`, this.cleanKey(jsonContent[key]))));
+			readme = readme
+				.replaceAll("{repo.name}", ctx.payload.repository.name)
+				.replaceAll("{repo.description}", ctx.payload.repository.description ?? "")
+				.replaceAll("{repo.license}", ctx.payload.repository.license?.spdx_id ?? "None");
+
+			await this.createCommit(ctx as any, readme, repo);
+		} catch (err) {
+			this.bot.logger.fatal(err);
+		}
 	}
 
 	private async configUpdate(ctx: Action.Context<"push">) {
@@ -42,7 +87,7 @@ export default class ReadmeSync extends Action {
 				.replaceAll("{repo.description}", ctx.payload.repository.description ?? "")
 				.replaceAll("{repo.license}", ctx.payload.repository.license?.spdx_id ?? "None");
 
-			await this.createCommit(ctx, readme, repo);
+			await this.createCommit(ctx as any, readme, repo);
 		} catch (err) {
 			this.bot.logger.fatal(err);
 		}
@@ -80,14 +125,18 @@ export default class ReadmeSync extends Action {
 					.replaceAll("{repo.description}", repository.description)
 					.replaceAll("{repo.license}", repository.license || "None");
 
-				await this.createCommit(ctx, updatedReadme, { owner: repository.owner, repo: repository.repo });
+				await this.createCommit(ctx as any, updatedReadme, { owner: repository.owner, repo: repository.repo });
 			} catch (err) {
 				this.bot.logger.fatal(err);
 			}
 		});
 	}
 
-	private async createCommit(ctx: Action.Context<"push">, content: string, repo: { owner: string; repo: string }) {
+	private async createCommit(
+		ctx: Action.Context<"push" | "repository.edited" | "repository.renamed">,
+		content: string,
+		repo: { owner: string; repo: string }
+	) {
 		const branchRef = `heads/${ctx.payload.repository.master_branch ?? "main"}`;
 		const { git } = ctx.octokit;
 
