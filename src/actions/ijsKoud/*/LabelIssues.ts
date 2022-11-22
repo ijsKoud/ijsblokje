7cb8b687e07d0e4d5fef81991619dc596d2c33cc
Daan Klarenbeek <github@ijskoud.dev>
2022-11-22 21:05:07 +0100

    feat: add auto-labeling (#61)
    * feat: add auto labeling for PRs and Issues
    
    * perf(LabelIssues): do not remove/add labels which already exist

---
+++ src/actions/ijsKoud/*/LabelIssues.ts
@@ -0,0 +1,43 @@
+import { ApplyActionOptions } from "../../../lib/Decorators/ActionDecorators.js";
+import { Action } from "../../../lib/Structures/Action.js";
+import { COMMIT_REGEX, COMMIT_TYPES, LABEL_ISSUES_EVENTS } from "../../../lib/constants.js";
+
+@ApplyActionOptions({
+	events: ["issues", "pull_request"]
+})
+export default class ReadmeSync extends Action {
+	public async run(ctx: Action.Context<"issues" | "pull_request">) {
+		const repo = ctx.repo();
+
+		if (!LABEL_ISSUES_EVENTS.includes(ctx.payload.action)) return;
+		if (ctx.payload.action === "opened" && ctx.name === "pull_request" && ctx.payload.sender.login === "renovate[bot]") {
+			const gLabels = this.bot.DataHandler.labels.get("global")!;
+			const label = gLabels.find((l) => l.name.toLowerCase().includes("Dependencies"));
+			if (label)
+				await ctx.octokit.issues.addLabels({
+					...repo,
+					issue_number: (ctx as any as Action.Context<"pull_request">).payload.pull_request.number,
+					labels: [label.name]
+				});
+		}
+
+		const title = "pull_request" in ctx.payload ? ctx.payload.pull_request.title : ctx.payload.issue.title;
+		const labels = "pull_request" in ctx.payload ? ctx.payload.pull_request.labels ?? [] : ctx.payload.issue.labels ?? [];
+		const issue = "pull_request" in ctx.payload ? ctx.payload.pull_request.number : ctx.payload.issue.number;
+
+		const conventionalCommitRes = COMMIT_REGEX.exec(title);
+		COMMIT_REGEX.lastIndex = 0;
+
+		const type = conventionalCommitRes?.groups!.type ?? "";
+
+		const gLabels = this.bot.DataHandler.labels.get("global")!;
+		const label = gLabels.find((l) => l.name.toLowerCase().includes(type));
+
+		const existingCommitLabels = labels.filter((label) => COMMIT_TYPES.some((t) => label.name.toLowerCase().includes(t)));
+		const filteredLabels = existingCommitLabels.filter((l) => l.name !== label?.name);
+		if (label && !existingCommitLabels.some((l) => l.name !== label?.name))
+			await ctx.octokit.issues.addLabels({ ...repo, issue_number: issue, labels: [label.name] });
+		if (filteredLabels.length)
+			await Promise.all(filteredLabels.map((label) => ctx.octokit.issues.removeLabel({ issue_number: issue, name: label.name, ...repo })));
+	}
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,27 @@
 export const REPO_UPDATE_EVENTS = ["deleted", "transferred"];
-export const README_CONFIG_LOCATION = ".github/.readmeconfig.json";
+export const LABEL_ISSUES_EVENTS = ["edited", "opened"];
 
+export const README_CONFIG_LOCATION = ".github/.readmeconfig.json";
 export const BASE_README = "config/readme_ijskoud.md";
 export const LABELS_CONFIG = "config/labels.json";
 
 export const GH_OWNER = "ijsKoud";
+
+export const COMMIT_TYPES = [
+	"chore",
+	"build",
+	"ci",
+	"docs",
+	"feat",
+	"fix",
+	"perf",
+	"refactor",
+	"revert",
+	"style",
+	"test",
+	"types",
+	"workflow",
+	"wip"
+];
+export const COMMIT_REGEX =
+	/^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|revert|style|test|¯\\_\(ツ\)_\/¯)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)/gm;
