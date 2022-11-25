e26784c537e02b082ac085d8e9dd34a1955d4605
ijsKoud <github@ijskoud.dev>
2022-11-25 18:39:22 +0100

    feat: add cd workflow runner for prs action

---
+++ src/actions/snowcrystals/*/NpmForPRs.ts
@@ -0,0 +1,24 @@
+import { ApplyActionOptions } from "../../../lib/Decorators/ActionDecorators.js";
+import { Action } from "../../../lib/Structures/Action.js";
+
+@ApplyActionOptions({
+	events: ["issue_comment", "workflow_run"]
+})
+export default class NpmForPRs extends Action {
+	public async run(ctx: Action.Context<"issue_comment" | "workflow_run">) {
+		if (ctx.name === "issue_comment") {
+			await this.pullRequest(ctx as any);
+			return;
+		}
+
+		// todo: code;
+		ctx;
+	}
+
+	private async pullRequest(ctx: Action.Context<"issue_comment">) {
+		if (ctx.payload.action !== "created") return;
+		console.log(ctx.payload.issue.draft);
+
+		await ctx;
+	}
+}
