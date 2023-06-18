b5d38b55609fdc12803594b57c16d54a25ec8673
Daan Klarenbeek <github@ijskoud.dev>
2023-06-18 15:27:08 +0200

    chore: remove old files

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { config } from "dotenv";
-import { join } from "node:path";
-config({ path: join(process.cwd(), "data", ".env") });
-
-import ijsblokje from "./lib/ijsBlokje.js";
-const bot = new ijsblokje();
-void bot.start();
