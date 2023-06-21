271ce0a0c6becd74244aa62f7a214839f7faa7dc
Daan Klarenbeek <github@ijskoud.dev>
2023-06-21 13:53:22 +0200

    feat(GitHubBot): update usage

--- apps/github-bot/src/index.ts
+++ apps/github-bot/src/index.ts
@@ -1,13 +1,13 @@
-import { config } from "dotenv";
-config();
-
+import "@ijsblokje/utils/env.js";
 import { Octocat } from "@ijsblokje/octocat";
 
 const bot = new Octocat({
 	appId: Number(process.env.APP_ID),
-	privateKey: process.env.PRIVATE_KEY as string,
-	clientId: "",
-	clientSecret: "",
-	redisUrl: ""
+	privateKey: process.env.PRIVATE_KEY,
+	clientId: process.env.GITHUB_CLIENT_ID,
+	clientSecret: process.env.GITHUB_CLIENT_SECRET,
+	redisUrl: process.env.REDIS_DATABASE_URL
 });
-void bot.start(3000);
+
+const urlOrPort = process.env.NODE_ENV === "production" ? Number(process.env.PORT) : process.env.SMEE_URL!;
+void bot.start(urlOrPort, process.env.WEBHOOK_SECRET);
