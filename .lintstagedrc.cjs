38e93e379922bf71e508aa43d46357fb1b41467e
Daan Klarenbeek <github@ijskoud.dev>
2023-06-22 20:38:01 +0200

    test(Release): add changelog tests

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,3 +1,3 @@
 module.exports = {
-	"**/*.{js,jsx,ts,tsx}": (filenames) => [`yarn run lint`, `prettier --write ${filenames.join(" ")}`]
+	"**/*.{js,jsx,ts,tsx}": (filenames) => ["yarn turbo lint test", `prettier --write ${filenames.join(" ")}`]
 };
--- packages/release/src/index.ts
+++ packages/release/src/index.ts
@@ -1,2 +1,3 @@
 export * from "./lib/Commit.js";
 export * from "./lib/VersionBump.js";
+export * from "./lib/Changelog.js";
