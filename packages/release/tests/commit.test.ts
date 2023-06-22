a37132fab00f088790455938ed18e1593dcafda5
Daan Klarenbeek <github@ijskoud.dev>
2023-06-22 14:39:04 +0200

    test(Release): add tests for commit and version bump

---
+++ packages/release/tests/commit.test.ts
@@ -0,0 +1,40 @@
+import { Commit, type CommitParserResult } from "../src/index.js";
+import { fixScopeMockCommit, fixMockCommit, featBreakingMockCommit } from "./mockdata.js";
+
+/** Checks whether the input is a class or not */
+function isClass(input: unknown) {
+	return typeof input === "function" && typeof input.prototype === "object";
+}
+
+describe("Commit", () => {
+	test("Commit should be a class", () => {
+		expect(isClass(Commit)).toBe(true);
+	});
+
+	test("constructor", () => {
+		const commit = new Commit(fixMockCommit);
+		expect(commit.data).toStrictEqual(fixMockCommit);
+	});
+
+	test("parse: scope", () => {
+		const commit = new Commit(fixScopeMockCommit);
+		expect(commit.parse()).toStrictEqual<CommitParserResult>({
+			breaking: false,
+			merge: false,
+			message: "all the bugs",
+			type: "fix",
+			scope: "Manager"
+		});
+	});
+
+	test("parse: breaking", () => {
+		const commit = new Commit(featBreakingMockCommit);
+		expect(commit.parse()).toStrictEqual<CommitParserResult>({
+			breaking: true,
+			merge: false,
+			message: "add better caching",
+			type: "feat",
+			scope: "Manager"
+		});
+	});
+});
