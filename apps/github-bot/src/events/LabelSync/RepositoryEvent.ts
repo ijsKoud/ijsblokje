461e7d4facfb70a55247db49f5bf2e5e8f1f5520
Daan Klarenbeek <github@ijskoud.dev>
2023-06-25 12:26:29 +0200

    feat: add label issues & label prs handlers

---
+++ apps/github-bot/src/events/LabelSync/RepositoryEvent.ts
@@ -0,0 +1,21 @@
+import { ApplyOptions, GitHubEvent, GitHubInstallation } from "@ijsblokje/octocat";
+import type { Octokit } from "@ijsblokje/octokit";
+import type { EmitterWebhookEvent } from "@ijsblokje/server";
+import { CreateLabel, DeleteLabel } from "./LabelSyncUtils.js";
+
+@ApplyOptions({ event: "repository.created" })
+export default class extends GitHubEvent {
+	public override async run(event: EmitterWebhookEvent<"repository.created">, octokit: Octokit, installation?: GitHubInstallation) {
+		if (!installation || !installation.defaultLabels) return;
+		const repoLabels = installation.labels.get(event.payload.repository.name) ?? [];
+		const labels = [...installation.defaultLabels, ...repoLabels];
+
+		const owner = installation.name;
+		const repo = event.payload.repository.name;
+
+		const existingLabels = await octokit.request("GET /repos/{owner}/{repo}/labels", { owner, repo }).catch(() => null);
+		if (existingLabels) await Promise.all(existingLabels.data.map((label) => DeleteLabel(octokit, label.name, owner, repo)));
+
+		await Promise.all(labels.map((label) => CreateLabel(octokit, label.name, label.color, label.description, owner, repo)));
+	}
+}
