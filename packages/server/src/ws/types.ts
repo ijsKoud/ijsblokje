69b6e5d790d96586759cabfe77c4ad8088414592
Daan Klarenbeek <github@ijskoud.dev>
2023-06-26 15:30:30 +0200

    feat(Server): add websocket server

---
+++ packages/server/src/ws/types.ts
@@ -0,0 +1,42 @@
+export enum WebsocketMessageType {
+	/** The ping event to check if the other connection is still alive */
+	PING = 1,
+
+	/** The release event to request a new release */
+	RELEASE = 2,
+
+	/** Event for a proposed version */
+	PROPOSED_VERSION = 3
+}
+
+export type WebsocketEvent<T extends WebsocketMessageType> = { t: T; d: unknown } & WebsocketEvents;
+export type WebsocketEvents = WebsocketPingEvent | WebsocketReleaseEvent | WebsocketVersionEvent;
+
+export interface WebsocketPingEvent {
+	t: WebsocketMessageType.PING;
+	d: "acknowledged" | "request";
+}
+
+export interface WebsocketReleaseEvent {
+	t: WebsocketMessageType.RELEASE;
+	d: {
+		owner: string;
+		repo: string;
+		version?: string;
+		message?: string;
+	};
+}
+
+export interface WebsocketVersionEvent {
+	t: WebsocketMessageType.PROPOSED_VERSION;
+	d:
+		| {
+				owner: string;
+				repo: string;
+		  }
+		| {
+				owner: string;
+				repo: string;
+				version: string | null | undefined;
+		  };
+}
