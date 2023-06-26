61ebc492bf68b8ed41fbc4bf5cb4edaaf87757c9
Daan Klarenbeek <github@ijskoud.dev>
2023-06-26 15:31:39 +0200

    feat(DiscordBot): add release command

--- apps/discord-bot/src/index.ts
+++ apps/discord-bot/src/index.ts
@@ -1,11 +1,11 @@
 import "@ijsblokje/utils/env/discord.js";
-import { IgloClient } from "@snowcrystals/iglo";
 import { GatewayIntentBits } from "discord.js";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
+import ExtendedIgloClient from "./lib/bot.js";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
-const bot = new IgloClient({
+const bot = new ExtendedIgloClient({
 	client: { intents: [GatewayIntentBits.Guilds] },
 	paths: { commands: join(__dirname, "commands"), events: join(__dirname, "events") }
 });
