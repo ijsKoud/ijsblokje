99458f9fbaf3e29f1a9e2b49e9e73128964d82ab
Daan Klarenbeek <github@ijskoud.dev>
2023-06-26 12:23:41 +0200

    feat(DiscordBot): add simple bot

--- apps/discord-bot/src/index.ts
+++ apps/discord-bot/src/index.ts
@@ -1,1 +1,13 @@
 import "@ijsblokje/utils/env/discord.js";
+import { IgloClient } from "@snowcrystals/iglo";
+import { GatewayIntentBits } from "discord.js";
+import { join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = fileURLToPath(new URL(".", import.meta.url));
+const bot = new IgloClient({
+	client: { intents: [GatewayIntentBits.Guilds] },
+	paths: { commands: join(__dirname, "commands"), events: join(__dirname, "events") }
+});
+
+void bot.run(process.env.DISCORD_TOKEN);
