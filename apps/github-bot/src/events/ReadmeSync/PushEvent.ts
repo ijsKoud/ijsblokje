b6f956a9575d91badd552fee20525fdd47bd0a0a
Daan Klarenbeek <github@ijskoud.dev>
2023-06-27 22:10:17 +0200

    fix(ReadmeSync): config file not updated

--- apps/github-bot/src/events/ReadmeSync/PushEvent.ts
+++ apps/github-bot/src/events/ReadmeSync/PushEvent.ts
@@ -27,21 +27,19 @@ export default class extends GitHubEvent {
 
 		const repo = event.payload.repository.name;
 		const owner = event.payload.repository.owner.login;
-		if (!config) {
-			const token = await octokit
-				.request("POST /app/installations/{installation_id}/access_tokens", {
-					installation_id: installation.installationId,
-					permissions: { contents: "read" }
-				})
-				.catch(() => null);
-			if (!token) return;
+		const token = await octokit
+			.request("POST /app/installations/{installation_id}/access_tokens", {
+				installation_id: installation.installationId,
+				permissions: { contents: "read" }
+			})
+			.catch(() => null);
+		if (!token) return;
 
-			const _config = await this.octocat.installations.getRepositoryConfig(token.data.token, owner, repo, event.payload.ref);
-			if (!_config) return;
+		const _config = await this.octocat.installations.getRepositoryConfig(token.data.token, owner, repo, event.payload.ref);
+		if (!_config) return;
 
-			config = ReadmeSync.getConfig(event.payload.repository as any, _config);
-			if (event.payload.ref.endsWith("/main")) installation.configs.set(repo, config);
-		}
+		config = ReadmeSync.getConfig(event.payload.repository as any, _config);
+		if (event.payload.ref.endsWith("/main")) installation.configs.set(repo, config);
 
 		if (config) await pushReadmeChanges(event, config, octokit, installation);
 	}
