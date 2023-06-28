4414b76049ae60a3409c5e5322d1132cd1dc16ff
Daan Klarenbeek <github@ijskoud.dev>
2023-06-28 14:02:33 +0200

    fix(ReadmeSync): incorrect object merging

--- packages/octocat/src/lib/structures/ReadmeSync.ts
+++ packages/octocat/src/lib/structures/ReadmeSync.ts
@@ -73,8 +73,8 @@ export class ReadmeSync {
 
 		try {
 			const parsedToml = parse(toml);
-			const mergedConfig = _.merge(parsedToml, baseConfig);
-			if (typeof parsedToml.repo === "object") mergedConfig.repo = { ...baseConfig.repo, ...parsedToml.repo };
+			const mergedConfig = _.merge(baseConfig, parsedToml);
+			// if (typeof parsedToml.repo === "object") mergedConfig.repo = { ...baseConfig.repo, ...parsedToml.repo };
 
 			const zodSchema = z.object({
 				repo: z.object({
